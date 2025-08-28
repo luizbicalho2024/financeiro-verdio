525f1111d50532b890284a63dc56e633325a0b07
Verdio - Rovema Bank <luiz.bicalho@rovemabank.com.br>
2025-08-27 20:28:57 -0400

    Update setup_admin.py

--- pages/setup_admin.py
+++ pages/setup_admin.py
@@ -1,12 +1,13 @@
-import streamlit as st
 import firebase_admin
-from firebase_admin import credentials, auth, firestore
+from firebase_admin import credentials, firestore, auth
+import streamlit as st
+import json
 
-# Inicializar Firebase apenas uma vez
 if not firebase_admin._apps:
-    cred = credentials.Certificate(dict(st.secrets["firebase_service_account"]))
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
-    db = firestore.client()
+db = firestore.client()
+
 
 st.title("🔑 Criar primeiro usuário Admin")
 
